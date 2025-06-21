439a861518e9ba3308cf254574a6e17b32b0d89f
Youkou Tenhouin <youkou@tenhou.in>
2025-06-21 19:21:18 +0200

    refactor: use Typed Racket for qr-code.rkt

--- qr-code.rkt
+++ qr-code.rkt
@@ -1,9 +1,10 @@
-#lang racket
+#lang typed/racket
 
 (provide qr-code)
 
-(require racket/system)
-(require simple-qr)
+(require typed/racket/system)
+(require/typed simple-qr
+               [qr-write (-> String Path-String Void)])
 
 (define qr-command "qrencode")
 
@@ -13,6 +14,11 @@
      temp-dir
      (string-append "bili-qr-login-" (number->string (current-milliseconds)) ".png"))))
 
+(define (qr-code-fail-message text)
+  (displayln "无法生生生生成二维码。")
+  (displayln "请手动将如下文本转换为二维码，并用Bilibili客户端扫描：")
+  (displayln text))
+
 (define (platform-file-opener)
   (find-executable-path
    (case (system-type 'os)
@@ -20,14 +26,19 @@
      [(unix) "xdg-open"]
      ; This is provided to me by Gemini. I don't own a Mac so I can't test it.
      ; If this ever blows up... Well it's your fault for using a Mac.
-     [(macos) "open"])))
+     [(macos) "open"]
+     [else (error "Unknown platform")])))
 
-(define (qr-code-fallback text)
-  (let ([temp-file (get-temp-file)])
-    (qr-write text temp-file)
-    (system* (platform-file-opener) temp-file)))
+(define (qr-code-fallback [text : String])
+  (let ([temp-file (get-temp-file)]
+        [file-opener (platform-file-opener)])
+    (if [and temp-file file-opener]
+        (begin
+          (qr-write text temp-file)
+          (system* file-opener temp-file))
+        (qr-code-fail-message text))))
 
-(define (qr-code text)
+(define (qr-code [text : String])
   (let ([qrencode (find-executable-path qr-command)])
     (if qrencode
         (system* qrencode "-t" "UTF8" text)
