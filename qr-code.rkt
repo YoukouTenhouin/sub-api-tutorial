b4e28124660eea6439ed17e116af433e07928c8a
Youkou Tenhouin <youkou@tenhou.in>
2025-06-21 10:42:28 +0200

    feat: implement fallback method for qr display

--- qr-code.rkt
+++ qr-code.rkt
@@ -3,16 +3,29 @@
 (provide qr-code)
 
 (require racket/system)
+(require simple-qr)
 
 (define qr-command "qrencode")
 
-(define (write-qr-text text)
-  (printf "系统上找不到`~a`命令。\n" qr-command)
-  (display "请手动将下列内容转换为二维码：\n")
-  (printf "~a\n" text))
+(define (get-temp-file)
+  (let ([temp-dir (find-system-path 'temp-dir)])
+    (build-path
+     temp-dir
+     (string-append "bili-qr-login-" (number->string (current-milliseconds)) ".png"))))
+
+(define (qr-code-fallback text)
+  (let ([temp-file (get-temp-file)])
+    (qr-write text temp-file)
+    (case (system-type 'os)
+      [(windows)
+       (let ([start (find-executable-path "start")])
+         (system* start temp-file))]
+      ([unix]
+       (let ([xdg-open (find-executable-path "xdg-open")])
+         (system* xdg-open temp-file))))))
 
 (define (qr-code text)
   (let ([qrencode (find-executable-path qr-command)])
     (if qrencode
         (system* qrencode "-t" "UTF8" text)
-        (write-qr-text text))))
+        (qr-code-fallback text))))
